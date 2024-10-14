c9b21c501e6791eecdfebe7d03348d551e94eccc
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-10-14 12:59:55 -0300

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        //Média calculada por 3 notas, tipo de comentário por linha
+        
+        /* Tipo de comentário
+        para diversas linhas
+         */
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                TOP GUN: MAVERICK
+                Filme de aventura
+                Ano de Lançamento 2022.
+                """;
+        System.out.println(sinopse);
     }
 }
